056e6cb0bc7a94bd6e81c17048efa48a1aa32b04
Artyom K <hotkosc@gmail.com>
2015-11-16 14:47:12 +0300

    Correct constraints and rename functions
    Thanks to @Minoru for help and patience.

--- Main.hs
+++ Main.hs
@@ -1,34 +1,33 @@
 import System.IO
 import Text.Read
 
-data Solution = 
-     OneSolution Double
-   | TwoSoulutions Double Double
+data Solution a =
+     OneSolution a
+   | TwoSolutions a a
    | NoSolutions deriving (Show)
 
-promptDouble :: Read a => String -> IO a
-promptDouble text = do
+prompt :: (Read a) => String -> IO a
+prompt text = do
     putStr text
     hFlush stdout
     line <- getLine
     case readMaybe line of
          Just x -> return x
-         Nothing -> putStrLn "Invalid number entered" >> promptDouble text
+         Nothing -> putStrLn "Invalid number entered" >> prompt text
 
-sqrtEquation :: Double -> Double -> Double -> Solution
-sqrtEquation a b c
+solveEquation :: (Floating a, Ord a) => a -> a -> a -> Solution a
+solveEquation a b c
     | d < 0.0 = NoSolutions
     | d == 0.0 = OneSolution x1
-    | d > 0.0 = TwoSoulutions x1 x2
+    | d > 0.0 = TwoSolutions x1 x2
     where d = b*b - 4*a*c
           x1 = f (+)
           x2 = f (-)
-          f operator = operator (-b) (sqrt d)/2*a
+          f operator = -b `operator` sqrt d / (2*a)
 
 main :: IO ()
 main = do
-        a <- promptDouble "Input a: "
-        b <- promptDouble "Input b: "
-        c <- promptDouble "Input c: "
-        let result = show (sqrtEquation a b c)
-        putStrLn result
+        a <- prompt "Input a: "
+        b <- prompt "Input b: "
+        c <- prompt "Input c: "
+        print $ solveEquation a b c
