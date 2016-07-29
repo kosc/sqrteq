e00666e0e47f70a27e98fa31618ece9724dc394c
Artyom K <hotkosc@gmail.com>
2016-07-29 17:54:02 +0300

    Getting correct solution where a==0

--- Main.hs
+++ Main.hs
@@ -17,6 +17,7 @@ prompt text = do
 
 solveEquation :: (Floating a, Ord a) => a -> a -> a -> Solution a
 solveEquation a b c
+    | a == 0.0 = OneSolution alone
     | d < 0.0 = NoSolutions
     | d == 0.0 = OneSolution x1
     | d > 0.0 = TwoSolutions x1 x2
@@ -24,6 +25,7 @@ solveEquation a b c
           x1 = f (+)
           x2 = f (-)
           f operator = -b `operator` sqrt d / (2*a)
+          alone = -c / b
 
 main :: IO ()
 main = do
